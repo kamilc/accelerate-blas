7306b9e04cc45c88439073b058111299a73f4733
Kamil Ciemniewski <kamil@endpoint.com>
2018-07-03 11:12:31 +0200

    Added a stub Backprop module and the compile flag

---
+++ src/Data/Array/Accelerate/Numeric/LinearAlgebra/Backprop.hs
@@ -0,0 +1,12 @@
+-- |
+-- Module      : Data.Array.Accelerate.Numeric.LinearAlgebra.Backprop
+-- Copyright   : [2018] Kamil Ciemniewski
+-- License     : BSD3
+--
+-- Maintainer  : Kamil Ciemniewski <kamil@ciemniew.ski>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+module Data.Array.Accelerate.Numeric.LinearAlgebra.Backprop where
+
+-- TODO: implement me
