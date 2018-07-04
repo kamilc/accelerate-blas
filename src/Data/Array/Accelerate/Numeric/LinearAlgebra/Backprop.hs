ed1153e5758b28c1a056d6b64589bf3306caa87a
Kamil Ciemniewski <kamil@endpoint.com>
2018-07-04 09:39:50 +0200

    Provided instances of Backprop for Scalar, Vector and Matrix

--- src/Data/Array/Accelerate/Numeric/LinearAlgebra/Backprop.hs
+++ src/Data/Array/Accelerate/Numeric/LinearAlgebra/Backprop.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE TypeSynonymInstances                     #-}
+{-# LANGUAGE FlexibleInstances                        #-}
+{-# LANGUAGE FlexibleContexts                         #-}
+{-# OPTIONS_GHC -fno-warn-orphans                     #-}
+
 -- |
 -- Module      : Data.Array.Accelerate.Numeric.LinearAlgebra.Backprop
 -- Copyright   : [2018] Kamil Ciemniewski
@@ -9,4 +14,21 @@
 --
 module Data.Array.Accelerate.Numeric.LinearAlgebra.Backprop where
 
--- TODO: implement me
+import           Numeric.Backprop
+import qualified Data.Array.Accelerate.Numeric.LinearAlgebra as L
+import qualified Data.Array.Accelerate as A
+
+instance (A.Elt n, Num n, Num (A.Exp n)) => Backprop (A.Acc (L.Scalar n)) where
+  zero s = A.fill (A.shape s) (A.constant 0)
+  one s = A.fill (A.shape s) (A.constant 1)
+  add l r = A.unit $ (A.the l) + (A.the r)
+
+instance (A.Elt n, Num n, Num (A.Exp n)) => Backprop (A.Acc (L.Vector n)) where
+  zero s = A.fill (A.shape s) (A.constant 0)
+  one s = A.fill (A.shape s) (A.constant 1)
+  add l r = A.zipWith (+) l r
+
+instance (A.Elt n, Num n, Num (A.Exp n)) => Backprop (A.Acc (L.Matrix n)) where
+  zero s = A.fill (A.shape s) (A.constant 0)
+  one s = A.fill (A.shape s) (A.constant 1)
+  add l r = A.zipWith (+) l r
