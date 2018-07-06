13f6788a2e5bd6b86645d1ea0f01a9ec8a983eac
Kamil Ciemniewski <kamil@endpoint.com>
2018-07-06 11:19:24 +0200

    Outer vector vector product fully implemented

--- src/Data/Array/Accelerate/Numeric/LinearAlgebra/Backprop.hs
+++ src/Data/Array/Accelerate/Numeric/LinearAlgebra/Backprop.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-orphans                     #-}
 
 -- |
@@ -13,11 +14,13 @@
 -- Stability   : experimental
 -- Portability : non-portable (GHC extensions)
 --
+
 module Data.Array.Accelerate.Numeric.LinearAlgebra.Backprop where
 
 import qualified Data.Array.Accelerate as A
 import qualified Data.Array.Accelerate.Numeric.LinearAlgebra as L
 import qualified Data.Array.Accelerate.Numeric.LinearAlgebra.BLAS.Level1 as L1
+import qualified Data.Array.Accelerate.Numeric.LinearAlgebra.BLAS.Level2 as L2
 import Numeric.Backprop
 
 type Scalar n = A.Acc (L.Scalar n)
@@ -32,29 +35,45 @@ instance (A.Shape sh, A.Elt n, Num n, Num (A.Exp n)) => Backprop (A.Acc (A.Array
   add = A.zipWith (+)
 
 -- | Vector - vector product
-(<.>) ::
-  (Reifies s W, Num (A.Exp n), L1.Numeric e, Num e)
+dotu ::
+  (Reifies s W, L1.Numeric e, Num e)
   => BVar s (Vector e)
   -> BVar s (Vector e)
   -> BVar s (Scalar e)
-(<.>) =
+dotu =
   liftOp2 . op2 $ \x y ->
     ( L1.dotu x y
     , \dzdy ->
         let d = A.the dzdy
          in (A.map (\a -> a * d) y, A.map (\a -> a * d) x))
+
+(<.>) ::
+  (Reifies s W, L1.Numeric e, Num e)
+  => BVar s (Vector e)
+  -> BVar s (Vector e)
+  -> BVar s (Scalar e)
+(<.>) = dotu
 infixr 8 <.>
 {-# INLINE (<.>) #-}
 
 -- | Outer vector - vector product
-(><) ::
-  (Reifies s W, Num (A.Exp n), L1.Numeric e, Num e)
+outer ::
+  forall s e. (Reifies s W, L1.Numeric e, Num e)
   => BVar s (Vector e)
   -> BVar s (Vector e)
   -> BVar s (Matrix e)
-(><) =
+outer =
   liftOp2 . op2 $ \x y ->
     ( (L.><) x y
-    , undefined )
+    , \dzdy ->
+        ((dzdy L.#> y), (L2.gemv (A.constant 1) L2.T dzdy x))
+      )
+
+(><) ::
+  forall s e. (Reifies s W, L1.Numeric e, Num e)
+  => BVar s (Vector e)
+  -> BVar s (Vector e)
+  -> BVar s (Matrix e)
+(><) = outer
 infixr 8 ><
 {-# INLINE (><) #-}
